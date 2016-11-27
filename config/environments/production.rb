c9c32eb7a94244ba734efa64330e052542008a5f
gregology <greg.clarke@shopify.com>
2016-11-27 16:33:06 -0500

    serving static files in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Serve static files
+  config.serve_static_assets = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
