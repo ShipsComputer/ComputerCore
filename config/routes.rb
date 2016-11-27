4bf354f09813ea7359c5cbcb8277814868118de0
gregology <greg.clarke@shopify.com>
2016-11-27 16:29:18 -0500

    adding static pages and lcarssdk

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :sensor_arrays
   resources :crafts
   resources :query
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "pages#show", page: "home"
+  get "/pages/:page" => "pages#show"
 end
