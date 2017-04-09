23ab9370671377c49ba08d3ec5a0bf7c93a93253
gregology <greg.clarke@shopify.com>
2017-04-09 06:33:38 -0400

    adding map

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 cats_away = Craft.create({name: 'Cats Away', handle: 'cats_away'})
 array     = cats_away.sensor_arrays.create({name: 'Core Pi', handle: 'core_pi'})
-location  = array.coordinates.create(latitude: 42, longitude: 24, timestamp: DateTime.now)
+location  = array.coordinates.create(latitude: 45, longitude: -75, timestamp: DateTime.now)
 weather   = array.weather_readings.create(temp: 25.0, humidity: 50, pressure: 1000)
